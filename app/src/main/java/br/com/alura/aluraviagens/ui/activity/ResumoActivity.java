c8754359f6f5ca4b2311c8e18078b07d20323665
jefferson <jefferson_1996Th@hotmail.com>
2019-10-01 21:55:26 -0300

    Programando itens de layout na classe

--- app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoActivity.java
+++ app/src/main/java/br/com/alura/aluraviagens/ui/activity/ResumoActivity.java
@@ -1,10 +1,17 @@
 package br.com.alura.aluraviagens.ui.activity;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import br.com.alura.aluraviagens.R;
 import br.com.alura.aluraviagens.model.Pacote;
+import br.com.alura.aluraviagens.util.DiasUtil;
+import br.com.alura.aluraviagens.util.MoedaUtil;
+import br.com.alura.aluraviagens.util.ResourceUtil;
 
 public class ResumoActivity extends AppCompatActivity {
 
@@ -14,6 +21,20 @@ public class ResumoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resumo);
 
         Pacote pacoteSaoPaulo = new Pacote("São Paulo", "sao_paulo_sp", 2, "243.99");
+
+        TextView textLocal = findViewById(R.id.resumo_pacote_local);
+        textLocal.setText(pacoteSaoPaulo.getLocal());
+
+        ImageView  imagem = findViewById(R.id.resumo_pacote_imagem);
+        Drawable drawableDoPacote = ResourceUtil.devolveDrawable(this,pacoteSaoPaulo.getImagem());
+        imagem.setImageDrawable(drawableDoPacote);
+
+        TextView diasEmTexto = findViewById(R.id.item_pacote_dias);
+        DiasUtil.formataEmTexto(pacoteSaoPaulo.getDias());
+        diasEmTexto.setText((CharSequence) diasEmTexto);
+
+        TextView preco = findViewById(R.id.resumo_pacote_preco);
+        String moedaBrasileira = MoedaUtil.formataParaBrasileiro(pacoteSaoPaulo.getPreco());
 
     }
 }
